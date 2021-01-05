3482f900a7398d6edf9a26e5a7dcfda24f0fe840
Brijesh Panchal <brijeshpanchal127@gmail.com>
2021-01-04 22:04:41 -0500

    implement place order functionality

--- express_filecrud/routes/index.js
+++ express_filecrud/routes/index.js
@@ -5,8 +5,8 @@ var MongoClient = require("mongodb").MongoClient;
 var url = "mongodb://localhost:27017/";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  
+router.get('/', function (req, res, next) {
+
   MongoClient.connect(url, { useUnifiedTopology: true },
     function (err, db) {
       if (err) {
@@ -14,67 +14,43 @@ router.get('/', function(req, res, next) {
       }
       else {
         var dbo = db.db("HABSTORES");
-        dbo.collection("Products").find().toArray( function (err, result) {
+        dbo.collection("Products").find().toArray(function (err, result) {
           if (err) {
             throw err;
           }
           else {
-            res.json({data:result});
+            res.json({ data: result });
             res.end();
             // db.close();
           }
         });
       }
     });
-  
-});
-
-
-//  C R (done) U (done)D 
-router.get('/readfile', function(req, res, next) {
-
-  console.log(req)
-  const fileContent = fs.readFileSync('/home/divya/workspace/stuff/nodejs/express_filecrud/test.txt',{encoding : 'utf8', flag:'r'});
-  res.json({ filecontent: fileContent})
-});
-
-
-
-
-
-router.post('/update', function(req, res, next) {
-
-  console.log(req)
-  try{
-    fs.appendFileSync('/home/divya/workspace/stuff/nodejs/express_filecrud/test.txt', req.body.data.vb.dfg.g, {encoding : 'utf8', flag:'a'});
-    res.json({ filecontent: "file read suss=cfuly"})
-  } catch(err){
-    res.status(500)
-    res.json({ filecontent: "file read unsuccefukt"})
-  }
 
 });
 
 
-router.post('/create', function(req, res, next) {
+router.patch("/placeorder", function (req, res, next) {
+  console.log(req.body.items[0].name);
 
-  var createStream = fs.createWriteStream(`/home/divya/workspace/stuff/nodejs/express_filecrud/${req.query.params}.txt`);
-  createStream.end(); 
-  res.json({ filecontent: "file created succfuly"})
-});
+  req.body.items.map(item => {
+    MongoClient.connect(url, { useUnifiedTopology: true },
+      function (err, db) {
+        if (err) {
+          throw err;
+        }
+        else {
+          var dbo = db.db("HABSTORES");
+          dbo.collection("Products").updateOne({ name: item.name },
+            {
+              $inc: { "stock": -1 }
+            });
 
+        }
 
-
-
-
-router.delete('/delete', function(req, res, next) {
-
-  fs.unlink(`/home/divya/workspace/stuff/nodejs/express_filecrud/${req.query.params}.txt`, (err) => {
-    if (err) throw err;
-    res.json({ filecontent: 'path/file.txt was deleted'})
+      });
 
   });
+  res.status(200).send("success");
 });
-
-
 module.exports = router;
